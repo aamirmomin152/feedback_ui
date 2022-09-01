80977a216837e402538cadf3cbee1461a733da26
Momin <aamirmomin152@gmail.com>
2022-08-31 20:36:03 -0500

    Dynamic values

--- feedback_ui/src/App.js
+++ feedback_ui/src/App.js
@@ -1,7 +1,11 @@
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is a blog post'
+
   return (
-    <div>
-      <h1 className="container">My App</h1>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
     </div>
   )
 }
