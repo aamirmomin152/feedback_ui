265536915c17d0acf6a49af7c3ea51157db2ba0d
Momin <aamirmomin152@gmail.com>
2022-08-31 20:48:48 -0500

    Conditionals

--- feedback_ui/src/App.js
+++ feedback_ui/src/App.js
@@ -7,18 +7,27 @@ function App() {
     {id: 1, text: 'Comment Three'}
   ]
 
+  const loading = false
+  const showComments = true
+  const commentBlock = (
+    <div className='comments'>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>comment.text</li>
+        ))}
+      </ul>
+    </div>
+  )
+
+  if (loading) return <h1>Loading...</h1>
+  
   return (
     <div className="container">
       <h1>{title}</h1>
       <p>{body}</p>
-      <div className='comments'>
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>comment.text</li>
-          ))}
-        </ul>
-      </div>
+
+      {showComments && commentBlock}
     </div>
   )
 }
