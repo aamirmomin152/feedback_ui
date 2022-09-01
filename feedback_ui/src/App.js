ca936b54135542e73634e1e4627003c4cd8692ba
Momin <aamirmomin152@gmail.com>
2022-08-31 20:39:33 -0500

    Dynamic Lists in JSX

--- feedback_ui/src/App.js
+++ feedback_ui/src/App.js
@@ -1,11 +1,24 @@
 function App() {
   const title = 'Blog Post'
   const body = 'This is a blog post'
+  const comments = [
+    {id: 1, text: 'Comment One'},
+    {id: 1, text: 'Comment Two'},
+    {id: 1, text: 'Comment Three'}
+  ]
 
   return (
     <div className="container">
       <h1>{title}</h1>
       <p>{body}</p>
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>comment.text</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
