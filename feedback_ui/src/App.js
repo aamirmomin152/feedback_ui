c2af0580e8bac2d60e13bc164392e652f6e8b3f7
Momin <aamirmomin152@gmail.com>
2022-09-07 20:25:05 -0500

    Testing changes

--- feedback_ui/src/App.js
+++ feedback_ui/src/App.js
@@ -1,33 +1,7 @@
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is a blog post'
-  const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 1, text: 'Comment Two'},
-    {id: 1, text: 'Comment Three'}
-  ]
-
-  const loading = false
-  const showComments = true
-  const commentBlock = (
-    <div className='comments'>
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>comment.text</li>
-        ))}
-      </ul>
-    </div>
-  )
-
-  if (loading) return <h1>Loading...</h1>
-  
   return (
     <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
+      <h1>Hi Aamir</h1>
     </div>
   )
 }
