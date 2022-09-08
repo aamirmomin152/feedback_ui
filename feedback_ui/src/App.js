6819a6eda9aa41ae960cbf4993f7d4e42a33b6ff
Momin <aamirmomin152@gmail.com>
2022-09-07 20:32:16 -0500

    Created first prop

--- feedback_ui/src/App.js
+++ feedback_ui/src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header"
 function App() {
   return (
-    <div className="container">
-      <h1>Hi Aamir</h1>
-    </div>
+    <>
+      <Header text='Hello World'/>
+      <div className="container">
+        <h1>Hi Aamir</h1>
+      </div>
+    </>
   )
 }
 
