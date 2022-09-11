ad32812906289ed8aba73b9e75c2d67e298cb368
Momin <aamirmomin152@gmail.com>
2022-09-11 14:18:41 -0500

    Conditional styling

--- feedback_ui/src/components/FeedbackItem.jsx
+++ feedback_ui/src/components/FeedbackItem.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import {useState} from 'react'
 import Card from './shared/Card'
 function FeedbackItem({item}) {
   return (
-    <Card reverse={true}>
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
     </Card>
