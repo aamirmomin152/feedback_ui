175e30072d8551c58a319b4d9814984dec00971f
Momin <aamirmomin152@gmail.com>
2022-09-11 16:39:42 -0500

    Filter out records when deleting

--- feedback_ui/src/components/FeedbackItem.jsx
+++ feedback_ui/src/components/FeedbackItem.jsx
@@ -3,11 +3,13 @@ import Card from './shared/Card'
 import PropTypes from 'prop-types'
 import {FaTimes} from 'react-icons/fa'
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
+
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className="close">
+      <button onClick={() => handleDelete(item.id)} className="close">
         <FaTimes color='purple'/>
       </button>
       <div className="text-display">{item.text}</div>
