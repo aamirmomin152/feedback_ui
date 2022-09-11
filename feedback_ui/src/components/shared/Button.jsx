cc815874e6cb7a076c048233d7431cf9a160fa72
Momin <aamirmomin152@gmail.com>
2022-09-11 17:19:05 -0500

    Built out reusable button component

---
+++ feedback_ui/src/components/shared/Button.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Button({children, version, type, isDisabled}) {
+  return (
+    <button type={type} disable={isDisabled} className={`btn btn-${version}`}>
+      {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  version: 'primary',
+  type: 'button',
+  isDisabled: false
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired, 
+  version: PropTypes.string, 
+  type: PropTypes.string, 
+  isDisabled: PropTypes.string
+}
+
+export default Button
