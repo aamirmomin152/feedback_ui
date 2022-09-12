b1b76bf51de00314748867928470b82d35e02db9
Momin <aamirmomin152@gmail.com>
2022-09-11 19:06:47 -0500

    Add feedback

--- feedback_ui/src/components/FeedbackForm.jsx
+++ feedback_ui/src/components/FeedbackForm.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Card from './shared/Card'
 import {useState} from 'react'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
@@ -24,12 +26,24 @@ function FeedbackForm() {
     setText(e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+
+      handleAdd(newFeedback)
+      setText('')
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-
-        {/* Rating Selector Component*/}
+        <RatingSelect select={setRating} selected={rating}/>
         <div className='input-group'>
           <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text} />
           <Button type='submit' isDisabled={btnDisabled}>Send</Button>
